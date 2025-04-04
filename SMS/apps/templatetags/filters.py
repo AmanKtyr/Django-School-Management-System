98165d6210b1a70354e7d1ceaeb022670baa55d8
Aman Ktyr <amanktyrrr@gmail.com>
2025-04-04 17:57:10 +0530

    make some changes in sub file

--- SMS/apps/templatetags/filters.py
+++ SMS/apps/templatetags/filters.py
@@ -9,6 +9,6 @@ register = template.Library()
 
 @register.inclusion_tag('dropdown_filter.html')
 def class_section_filter():
-    classes = current_class.objects.all()  # Assuming you have a Class model
-    sections = Section.objects.all()  # Assuming you have a Section model
+    classes = current_class.objects.all()  
+    sections = Section.objects.all()  
     return {'classes': classes, 'sections': sections}
