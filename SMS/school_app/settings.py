d98a34b2cbc1ce806b42f61d55cdd9a209546402
Aman Ktyr <amanktyrrr@gmail.com>
2025-05-29 18:03:25 +0530

    Add setuptools dependency and configure pymysql for MySQL support

--- SMS/school_app/settings.py
+++ SMS/school_app/settings.py
@@ -14,6 +14,10 @@ import os
 import dj_database_url
 from pathlib import Path
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
